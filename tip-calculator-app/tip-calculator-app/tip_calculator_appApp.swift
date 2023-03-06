d9718f056bd912c4e69718bba43a6761f1d51fe5
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-06 00:24:59 -0800

    created SwiftUI project

---
+++ tip-calculator-app/tip-calculator-app/tip_calculator_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tip_calculator_appApp.swift
+//  tip-calculator-app
+//
+//  Created by Jonathan Bigbee on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct tip_calculator_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
